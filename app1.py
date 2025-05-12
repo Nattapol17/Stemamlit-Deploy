0b0f569341ab0122dbfda3a3a0c9f0daa1b68226
Nattapol17 <natta6191@gmail.com>
2025-05-12 08:53:26 -0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -67,7 +67,7 @@ with tab4:
 with tab3:
     st.markdown("<h1 style='text-align: center; font-size: 30px;'>🏭 Inventory Status</h1>", unsafe_allow_html=True)
     st_autorefresh(interval=2000, key="datarefresh")
-    url = 'https://common-carpets-fetch.loca.lt/data'
+    url = 'https://lovely-ants-walk.loca.lt/data'
     def led_indicator(value):
         if value == 1:
             color = "red"
